96115334ce8c6eb7919d69a2e320788b36b542b9
Felipe Hidequel <felipehidequel@gmail.com>
2022-12-19 14:23:55 -0300

    included screen authenticate fuction

--- agenda/urls.py
+++ agenda/urls.py
@@ -18,8 +18,11 @@ from django.urls import path
 from core import views
 from django.views.generic import RedirectView
 
-urlpatterns = [
+urlpatterns = [    
     path('admin/', admin.site.urls),
-    path('agenda/', views.lista_eventos)
-    # path('', RedirectView.as_view(url='/agenda/'))
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user, name='login'),   
+    path('login/submit/', views.submit_login, name='submit'),
+    path('logout/', views.logout_user, name='logout')
 ]
