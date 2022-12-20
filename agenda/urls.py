8d7c7fa5fdfeb127d0288be1905982659f7277ef
Felipe Hidequel <felipehidequel@gmail.com>
2022-12-19 23:44:49 -0300

    Included template for inserting data in the agenda. Included gitignore for ignore files compiled

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,8 +21,10 @@ from django.views.generic import RedirectView
 urlpatterns = [    
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user, name='login'),   
-    path('login/submit/', views.submit_login, name='submit'),
+    path('login/submit', views.submit_login, name='submit'),
     path('logout/', views.logout_user, name='logout')
 ]
